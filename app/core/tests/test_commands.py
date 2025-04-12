10bb7d553a2f0cbfc843615f7ab551fe992eb1cf
shai1801 <shai1801@gmail.com>
2025-04-13 00:33:41 +0100

    DB config race condition

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,20 @@
+"""
+Test custom Django management commands. 
+"""
+
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+
+class CommandTests(SimpleTestCase):
+    """Test Commands."""
+    
+    def test_wait_for_db_ready(self, patched_check):
+        """Testing waiting for db if db is ready"""
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(database=['default'])
