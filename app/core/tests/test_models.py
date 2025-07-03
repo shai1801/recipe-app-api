7c646c75a4cbd25600a8ea6ea9fb8f345af13560
shai1801 <shai1801@gmail.com>
2025-07-03 23:32:51 +0100

    Ingredient model created.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -86,6 +86,9 @@ class ModelTests(TestCase):
         """Test creation ingredient."""
 
         user = create_user()
-        ingredient = models.Ingredient.objects.create(user=user, name='Ingredient1')
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
 
         self.assertEqual(str(ingredient), ingredient.name)
