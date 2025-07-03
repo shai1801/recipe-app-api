dfe50b7337f06c1d1d42bc43da3f58c630e2c42d
shai1801 <shai1801@gmail.com>
2025-07-04 00:32:23 +0100

    Basic structure for Ingredient Recipe implemented

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,11 @@ from rest_framework import (
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import (Recipe, Tag)
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+    )
 from recipe import serializers
 
 
@@ -50,4 +54,20 @@ class TagViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet
+                        ):
+    """Manage ingredients in database."""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user and order by name."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
