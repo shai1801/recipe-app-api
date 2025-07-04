5c89ebb8c72e8edb6e0ab29d69d6379869ce5397
shai1801 <shai1801@gmail.com>
2025-07-04 12:17:44 +0100

    added recipe delete feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -58,6 +58,7 @@ class TagViewSet(
 
 
 class IngredientViewSet(
+                        mixins.DestroyModelMixin,
                         mixins.UpdateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet
