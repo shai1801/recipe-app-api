663af5b731649bb7435e122dda60a07418fb4cee
shai1801 <shai1801@gmail.com>
2025-07-12 14:34:48 +0100

    added health check feature

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,10 +21,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/helath-check', core_views.health_check, name='health-check'),
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
